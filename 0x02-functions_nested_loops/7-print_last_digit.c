b3bbfd1466ccc6ec994164dd97712aa45e1aca6f
Mr-Lunga <gen.lunga@gmail.com>
2022-06-08 04:53:30 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digi - function prototype
+ * @n: The number to check
+ * Description: Prints the last digit of a number
+ * Return: The last digit of the number
+ */
+int print_last_digit(int n)
+{
+	n %= 10;
+
+	if (n > 0)
+		n = -n;
+	_putchar(n + '0');
+
+	return (n);
+}

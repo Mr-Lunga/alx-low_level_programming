895a3bc2876a837e01116e3a4954cf573bec18a1
Mr-Lunga <gen.lunga@gmail.com>
2022-07-05 12:00:49 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,21 +1,13 @@
 #ifndef _DOG_H_
-
 #define _DOG_H_
 
 /**
  * struct dog -Dog data.
-
  * @name: Name of the dog
-
  * @age: age of the dog.
-
  * @owner: owner of the dog.
-
  * Desctiption: a structure with the name, age and owner of a dog.
-
 **/
-
-
 
 typedef struct dog
 
